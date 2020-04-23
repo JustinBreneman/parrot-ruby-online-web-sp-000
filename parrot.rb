ed914a0eb4bbe0bacdbaee0416b3092b5cdb05b0
Justin Breneman <justin.breneman@yahoo.com>
2020-04-23 23:34:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(wording = "Sqwuak")
-  puts "#{wording}!"
+def parrot(wording = "Sqwuak!")
+  puts "#{wording}"
   phrase = wording
 end
